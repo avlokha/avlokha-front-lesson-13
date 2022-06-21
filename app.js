3b5a80acbeda2d5ca9d5f9eea47ab947e9e224f5
avlokha <avlokhashvili93@gmail.com>
2022-06-21 17:31:13 +0400

    add html css and js

---
+++ app.js
@@ -0,0 +1,18 @@
+let userName;
+let userHobby;
+let ueserAdress;
+let userEmail;
+let yearOfBirth;
+
+let myNameIS = "nika";
+let YearOfBirth = 1994;
+let currentYear = 2022;
+let myHobby = "fishing";
+
+console.log(myNameIS);
+console.log(YearOfBirth);
+console.log(currentYear);
+console.log(myHobby);
+
+let iam = currentYear - YearOfBirth;
+console.log("iam", iam);
